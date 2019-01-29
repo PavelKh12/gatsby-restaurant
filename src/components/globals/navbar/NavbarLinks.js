4838b504c38a798139bc723e9bd40ed8debcbc26
Pavel <kharlamovpavell@gmail.com>
2019-01-29 16:38:12 +0300

    nav added

---
+++ src/components/globals/navbar/NavbarLinks.js
@@ -0,0 +1,87 @@
+import React, {Component} from 'react';
+import styled from 'styled-components'
+import {Link} from 'gatsby'
+import {styles} from '../../../utils'
+
+class NavbarLinks extends Component {
+    state = {
+        links: [
+            {
+                id: 0,
+                path: '/',
+                name: 'home'
+            },
+            {
+                id: 1,
+                path: '/about/',
+                name: 'about'
+            },
+            {
+                id: 2,
+                path: '/menu',
+                name: 'menu'
+            },
+            {
+                id: 3,
+                path: '/contact',
+                name: 'contact'
+            }
+
+        ]
+    }
+
+    render() {
+        return (
+            <LinkWrapper open={this.props.navbarOpen}>
+                {this.state.links.map(item => {
+                    return (<li key={item.id}>
+                            <Link to={item.path} className="nav-link">{item.name}</Link>
+                        </li>
+                    )
+                })
+                }
+            </LinkWrapper>
+        )
+            ;
+    }
+}
+
+const LinkWrapper = styled.ul`
+li {
+list-styled-type:none;
+}
+.nav-link{
+display:block;
+text-decoration:none;
+padding: .5rem 1rem .5rem 1rem;
+color: ${styles.colors.mainGrey};
+font-weight:700;
+text-transform: capitalize;
+cursor:pointer;
+${styles.transDefault};
+transition:all .5s linear;
+&:hover {
+background: ${styles.colors.mainGrey};
+color: ${styles.colors.mainYellow};
+padding: .5rem 1rem .5rem 1.3rem
+}
+}
+height:${props => (props.open ? '152px' : '0px')};
+overflow:hidden;
+${styles.transObject({time: '1s'})};
+@media (min-width:768px) {
+height: auto;
+display: flex;
+margin: 0 auto;
+.nav-link:hover {
+background: ${styles.colors.mainWhite};
+padding: .5rem 1rem .5rem 1rem;
+
+}
+
+}
+
+
+`
+
+export default NavbarLinks;
--- src/pages/about.js
+++ src/pages/about.js
@@ -4,11 +4,12 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
+const AboutPage = () => (
     <Layout>
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
+        <h3>this is about page</h3>
     </Layout>
 )
 
 
-export default IndexPage
+export default AboutPage
